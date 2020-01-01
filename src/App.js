25d2b04d8a93eb0eae7cd353dd32c1fd450e20c4
Blade Boles <bladerboles@gmail.com>
2020-01-01 10:45:04 -0700

    Worked on Cat, Participant, and Stage components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Participant from './Participant';
-
+import Stage from './Stage';
+import Chat from './Chat';
 
 
 function App(props) {
     
   return (
+    <div>
     <Participant />
+    <Stage />
+    <Chat />
+    </div>
   );
 }
 
---
+++ src/Stage.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const participants = [
+  {
+      id: 1,
+      name: 'Koren Templeton',
+      avatar:
+          'https://robohash.org/itaquevoluptatumrerum.jpg?size=200x200&set=set1',
+      inSession: true,
+      onStage: true
+  },
+  {
+      id: 2,
+      name: 'Caty Flucker',
+      avatar:
+          'https://robohash.org/doloredolorescupiditate.jpg?size=200x200&set=set1',
+      inSession: true,
+      onStage: false
+  },
+  {
+      id: 3,
+      name: 'Axe Kubicka',
+      avatar: 'https://robohash.org/utlaborumfugit.jpg?size=200x200&set=set1',
+      inSession: false,
+      onStage: false
+  },
+  {
+      id: 4,
+      name: 'Frank Runciman',
+      avatar:
+          'https://robohash.org/etexercitationemassumenda.jpg?size=200x200&set=set1',
+      inSession: true,
+      onStage: false
+  },
+  {
+      id: 5,
+      name: 'Ashla Attwool',
+      avatar:
+          'https://robohash.org/iustodoloremqueinventore.jpg?size=200x200&set=set1',
+      inSession: true,
+      onStage: true
+  }
+];
+
+
+export default function Stage(props) {
+const stageArray = participants.map(person => {  
+  let onStageo = 'yes';
+  if (person.onStage === false) {
+          return;
+      }
+      return (
+      <div>
+      <h3>Name: {person.name}</h3>
+      <img src ={person.avatar} alt='alt text' />
+      </div>)
+})
+return (
+  <div>
+      {stageArray}
+  </div>
+)
+}
